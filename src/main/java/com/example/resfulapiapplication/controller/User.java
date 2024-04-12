7db130c5d817d5c80f254179741586700d8a77b7
Rains97 <hvinhtuong@gmail.com>
2024-04-12 18:52:15 +0700

    Init project

---
+++ src/main/java/com/example/resfulapiapplication/controller/User.java
@@ -0,0 +1,16 @@
+package com.example.resfulapiapplication.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    public int id;
+    public String name;
+}
+
---
+++ src/test/java/com/example/resfulapiapplication/ResfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resfulapiapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResfulApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
